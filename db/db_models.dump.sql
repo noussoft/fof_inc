9dd42af6e127b70ca27300313dd8c7cd50c5169f
groomycian <groomycian@gmail.com>
2017-01-10 17:15:13 +0300

    * added database models in MySQL DDL

---
+++ db/db_models.dump.sql
@@ -0,0 +1,149 @@
+-- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: fof_db
+-- ------------------------------------------------------
+-- Server version	5.5.53-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `guid` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `subtitle` varchar(255) DEFAULT NULL,
+  `scraped` datetime NOT NULL,
+  `posted` datetime DEFAULT NULL,
+  `publication_id` int(11) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `photo1_url` varchar(255) DEFAULT NULL,
+  `photo1_data` blob,
+  `photo2_url` varchar(255) DEFAULT NULL,
+  `photo2_data` blob,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ix_articles_guid` (`guid`),
+  KEY `publication_id` (`publication_id`),
+  KEY `ix_articles_posted` (`posted`),
+  KEY `ix_articles_subtitle` (`subtitle`),
+  KEY `ix_articles_title` (`title`),
+  CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`publication_id`) REFERENCES `publications` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `authors`
+--
+
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first` varchar(32) NOT NULL,
+  `last` varchar(32) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `twitter` varchar(255) DEFAULT NULL,
+  `facebook` varchar(255) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `articles_page` varchar(255) DEFAULT NULL,
+  `bio` text,
+  PRIMARY KEY (`id`),
+  KEY `ix_authors_first` (`first`),
+  KEY `ix_authors_email` (`email`),
+  KEY `ix_authors_last` (`last`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `authors_articles`
+--
+
+DROP TABLE IF EXISTS `authors_articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors_articles` (
+  `author_id` int(11) DEFAULT NULL,
+  `article_id` int(11) DEFAULT NULL,
+  `primary_author_yn` tinyint(1) DEFAULT NULL,
+  KEY `author_id` (`author_id`),
+  KEY `article_id` (`article_id`),
+  CONSTRAINT `authors_articles_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`),
+  CONSTRAINT `authors_articles_ibfk_2` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `publications`
+--
+
+DROP TABLE IF EXISTS `publications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `mobile_url` varchar(255) DEFAULT NULL,
+  `rss_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_publications_title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `text` (`text`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tags_articles`
+--
+
+DROP TABLE IF EXISTS `tags_articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags_articles` (
+  `tag_id` int(11) DEFAULT NULL,
+  `article_id` int(11) DEFAULT NULL,
+  KEY `tag_id` (`tag_id`),
+  KEY `article_id` (`article_id`),
+  CONSTRAINT `tags_articles_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`),
+  CONSTRAINT `tags_articles_ibfk_2` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-10 17:13:14
